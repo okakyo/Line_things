11323564be1d2999231b834444643d281d9f82bf
Kyohei Oka <0622okakyo@gmail.com>
2019-02-02 14:01:47 +0900

    '確認中'

--- get.py
+++ get.py
@@ -1,7 +1,6 @@
 import requests
 
-
-channel_token=''
+channel_token='ZC8x49i2Pcmj5il0zeLzRkT0PRfYKz3+VTYYCygGfwpXiLiGjkPDe17QF8Ri7tS3vTaoApzTblDlc1Efo/vEHO8ZEmiGklVvC9XWLXMPcJiGXhIOM5rWJr97bf6OoDhOeQ45GXpkKZV1X5s+YOyP+FGUYhWQfeY8sLGRXgo3xvw='
 header={'Authorization':'Bearer {}'.format(channel_token)}
 
 data=requests.get('https://api.line.me/things/v1/trial/products',headers=header)
