4268372e3d2cfcf61e4b7aa9616a785ccc993c92
Kyohei Oka <0622okakyo@gmail.com>
2019-02-07 18:56:27 +0900

    'Arduino確認中'

--- Arduino_Sample/Arduino_Sample.ino
+++ Arduino_Sample/Arduino_Sample.ino
@@ -39,6 +39,68 @@ BLECharacteristic *notifyCharacteristic;
 bool deviceConnected=false;
 bool oldDeviceConnected=false;
 
+void left_motor(){
+  digitalWrite();
+  digitalWrite();
+}
+void right_motor(){
+  digitalWrite();
+  digitalWrite();
+}
+
+void forward(){
+  left_motor();
+  right_motor();
+}
+void back(){
+  left_motor();
+  right_motor();
+}
+void left(){
+  left_motor();
+  right_motor();
+}
+void right(){
+  left_motor();
+  right_motor();
+}
+
+void controller(signal){
+  switch (signal){
+    case 'l':{
+      left();
+    }
+    case 'r':{
+      right();
+    }
+    case 'f':{
+      forward();
+    }
+    case 'b':{
+      back();
+    }
+  }
+}
+
+class serverCallbacks public BLEServerCallbacks{
+  void onConnect(BLEServer *pServer){
+    deviceConnected=true;
+  }
+
+  void onDisconnecct(BLEServer *pServer){
+    deviceConnected=false;        
+  }
+};
+
+class writeCallback public BLECharacteristicCallbacks{
+  void onWrite(BLECharacteristic *bleWriteCharacteristic){
+    std::string value bleWriteCharacteristic->getValue();
+    if((char)value[0]<=1){
+        digitalWrite(LED1,(char)value[0])      
+      }
+    }
+};
+
 void setup() {
   Serial.begin(115200);
   
@@ -47,12 +109,28 @@ void setup() {
   pinMode(MOTOR_3,OUTPUT);
   pinMode(MOTOR_4,OUTPUT);
   
+  BLEDevice::init("");
+  BLEDevice::setEncryptionLevel(ESP_SEC_ENCRYPT_NO_MTIM);
+
+  BLESecurity *thingsSecurity=new BLESecurity();
+  thingsSecurity->setAuthenticationNode(ESP_LE_AUTH_REQ_SC_ONLY);
+  thingsSecurity->setCapability(ESP_IO_CAP_NONE);
+  thingsSecurity->setInitEncryptionKey(ESP_BLE_ENC_KEY_MASK|ESP_BLE_ID_KEY_MASK);
+  
   setupServices();
   startAdvertising();
+  Serial.println("Ready to Connect");
 }
 
 void loop() {
-   delay(500);
+   char signal;
+   if(!deviveConnected &&oldDeviceConnected){
+     delay(500);
+     thingsServer->startAdvertising();
+     oldDeviceConnected=deviceConnected;
+   }
+   if(deviceConnected &&!oldDeviceConnected)
+    oldDeviceConnected=deviceConnected;
 }
 
 //Write the two functions as almost all the same ways as examples
